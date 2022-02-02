96724d9e1450527cd3081a169409cb9c547df21a
PeterGreen8801 <petergreen880@yahoo.com>
2022-02-02 16:12:47 -0500

    Changed Sockets.
    Made bun have multiple sockets for each ingredient instead of ingredients having their own sockets. Will split between top and bottom bun for ingredients and patty.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/SocketWithTagCheck.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/SocketWithTagCheck.cs
@@ -6,15 +6,20 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class SocketWithTagCheck : XRSocketInteractor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public string targetTag = string.Empty;
 
+    public override bool CanHover(XRBaseInteractable interactable)
+    {
+        return base.CanHover(interactable) && MatchUsingTag(interactable);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool CanSelect(XRBaseInteractable interactable)
     {
+        return base.CanSelect(interactable) && MatchUsingTag(interactable);
+    }
 
+    private bool MatchUsingTag(XRBaseInteractable interactable)
+    {
+        return interactable.CompareTag(targetTag);
     }
 }
