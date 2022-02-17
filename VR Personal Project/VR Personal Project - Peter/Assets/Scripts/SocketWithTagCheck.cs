82c6d4d6a244a1b83542b6d42b2f842cdaf1146e
PeterGreen8801 <69487518+PeterGreen8801@users.noreply.github.com>
2022-02-17 15:57:37 -0500

    Update SocketWithTagCheck.cs

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/SocketWithTagCheck.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/SocketWithTagCheck.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-
 public class SocketWithTagCheck : XRSocketInteractor
 {
     public string targetTag = string.Empty;
