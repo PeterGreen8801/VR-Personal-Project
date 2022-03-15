0f369174ebeab05076c1657bb93c7928544edd0a
PeterGreen8801 <petergreen880@yahoo.com>
2022-03-14 21:58:29 -0400

    Cooked Patty Spawner
    Added Cooked Patty Spawner on grill socket. Waits 5 seconds when an uncooked patty is placed on the socket, and then instantiates a cooked patty on the attach point.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/CreateObject.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/CreateObject.cs
@@ -7,8 +7,32 @@ public class CreateObject : MonoBehaviour
     public Transform Spawnpoint;
     public GameObject Prefab;
 
+    public float nextSpawnTime;
+    public float spawnDelay = 5f;
+
+    public void Spawn()
+    {
+        if (shouldSpawn())
+        {
+            IEnumerator Wait()
+            {
+                //nextSpawnTime = Time.deltaTime + spawnDelay;
+                yield return new WaitForSeconds(spawnDelay);
+                Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+            }
+            StartCoroutine(Wait());
+        }
+    }
+
+    private bool shouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+
+    /*
     void OnTriggerEnter()
     {
         Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
     }
+    */
 }
