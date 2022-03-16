d3b98fde6caf608482d3ee0bc9588c6cc8a1278b
PeterGreen8801 <petergreen880@yahoo.com>
2022-03-16 17:41:52 -0400

    Added functionality to grill sockets
    Added same functionality to grill sockets. Sockets 1-3 now cook patties.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/TurnOffGrab.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/TurnOffGrab.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class TurnOffGrab : MonoBehaviour
 {
     public bool canGrab = true;
 
+    XRGrabInteractable pattygrab;
+
+    public float disableDelay = 5f;
+
+    void Start()
+    {
+        pattygrab = GetComponent<XRGrabInteractable>();
+    }
+
     [SerializeField] GameObject uncooked;
 
     public void turnOffGrab()
     {
-
+        pattygrab.enabled = false;
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(disableDelay);
+            pattygrab.enabled = true;
+            Debug.Log("Socket turned ON!");
+        }
+        StartCoroutine(Wait());
     }
 
 }
