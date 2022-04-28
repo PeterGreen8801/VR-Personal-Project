96e5bf779663ee87b92331622e02d73c2b2b34c9
PeterGreen8801 <petergreen880@yahoo.com>
2022-04-27 22:16:31 -0400

    Added Better Timer
    Added public start timer method and int textHolderToUse Parameter, so each Grill Socket can give an int which will turn on the correct Holder/Text for the spot on the grill.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown.cs
@@ -9,11 +9,18 @@ public class Countdown : MonoBehaviour
     public float currentTime = 0f;
     public float startingTime = 5f;
 
+    public bool timerIsRunning = false;
+
+    public int holderPicker = 0;
+
     [SerializeField]
     TextMeshProUGUI countdownText;
 
     [SerializeField]
     GameObject textHolder;
+
+    [SerializeField]
+    GameObject textHolder2;
 
     void Start()
     {
@@ -33,15 +40,43 @@ public class Countdown : MonoBehaviour
     }
     */
 
-    void startCountdown()
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (holderPicker == 1)
+            {
+                textHolder.SetActive(true);
+                currentTime = currentTime - 1 * Time.deltaTime;
+                countdownText.text = currentTime.ToString("0");
+
+                if (currentTime <= 0)
+                {
+                    currentTime = startingTime;
+                    textHolder.SetActive(false);
+                    timerIsRunning = false;
+                }
+            }
+
+        }
+    }
+
+    public void startCountdown()
     {
         textHolder.SetActive(true);
-        currentTime -= 1 * Time.deltaTime;
+        currentTime = currentTime - 1 * Time.deltaTime;
         countdownText.text = currentTime.ToString("0");
 
         if (currentTime <= 0)
         {
             currentTime = 0;
+            textHolder.SetActive(false);
         }
+    }
+
+    public void startTimer(int textHolderToUse)
+    {
+        timerIsRunning = true;
+        holderPicker = textHolderToUse;
     }
 }
