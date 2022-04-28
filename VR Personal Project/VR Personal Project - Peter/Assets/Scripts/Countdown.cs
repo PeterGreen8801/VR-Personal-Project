ba85f2895f562eb68685a17aaa23f029a67e8bab
PeterGreen8801 <petergreen880@yahoo.com>
2022-04-27 21:38:08 -0400

    Added Countdown text
    Added countdown text timer. Need to place it over the grill sockets and have it activate once a patty is put down.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Countdown : MonoBehaviour
 {
     public float currentTime = 0f;
-    public float startingTime = 10f;
+    public float startingTime = 5f;
+
+    [SerializeField]
+    TextMeshProUGUI countdownText;
+
+    [SerializeField]
+    GameObject textHolder;
 
     void Start()
     {
         currentTime = startingTime;
     }
 
+    /*
     void Update()
     {
         currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+    */
+
+    void startCountdown()
+    {
+        textHolder.SetActive(true);
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
     }
 }
