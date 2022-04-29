fd5b687f883535276524c5679cc3fae4b83001fb
PeterGreen8801 <petergreen880@yahoo.com>
2022-04-28 20:55:48 -0400

    Updated Countdown2 Timer
    Made Countdown2 Timer the same as Countdown. Works well.

--- VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown2.cs
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown2.cs
@@ -9,81 +9,55 @@ public class Countdown2 : MonoBehaviour
     public float currentTime = 0f;
     public float startingTime = 5f;
 
-    public float currentTime2 = 0f;
-
-
     public bool timerIsRunning = false;
 
-    public bool timer2IsRunning = false;
-
-    public int holderPicker = 0;
+    //public int holderPicker = 0;
 
     [SerializeField]
     TextMeshProUGUI countdownText;
 
     [SerializeField]
-    TextMeshProUGUI countdownText2;
-
-    [SerializeField]
     GameObject textHolder;
-
-    [SerializeField]
-    GameObject textHolder2;
 
     void Start()
     {
         currentTime = startingTime;
-        currentTime2 = startingTime;
     }
 
-    /*
-    void Update()
-    {
-        currentTime -= 1 * Time.deltaTime;
-        countdownText.text = currentTime.ToString("0");
-
-        if (currentTime <= 0)
-        {
-            currentTime = 0;
-        }
-    }
-    */
 
     void Update()
     {
-        if (timer2IsRunning)
+        if (timerIsRunning)
         {
-            if (holderPicker == 2)
+            textHolder.SetActive(true);
+            currentTime = currentTime - 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
+
+            if (currentTime <= 0)
             {
-                textHolder2.SetActive(true);
-                currentTime2 = currentTime2 - 1 * Time.deltaTime;
-                countdownText2.text = currentTime2.ToString("0");
-
-                if (currentTime2 <= 0)
-                {
-                    currentTime2 = startingTime;
-                    textHolder2.SetActive(false);
-                    timer2IsRunning = false;
-                }
+                currentTime = startingTime;
+                textHolder.SetActive(false);
+                timerIsRunning = false;
             }
         }
+
     }
 
 
-
-    public void startTimer(int textHolderToUse)
+    /*
+    public void startTime(int textHolderToUse)
     {
         if (textHolderToUse == 1)
         {
             timerIsRunning = true;
-            holderPicker = textHolderToUse;
-        }
-        if (textHolderToUse == 2)
-        {
-            timer2IsRunning = true;
-            holderPicker = textHolderToUse;
+            //holderPicker = textHolderToUse;
         }
 
+    }
+    */
 
+    public void startTimer()
+    {
+        timerIsRunning = true;
     }
 }
