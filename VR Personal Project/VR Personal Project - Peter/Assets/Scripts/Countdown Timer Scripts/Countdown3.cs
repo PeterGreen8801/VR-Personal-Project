9040d498bc60d39a257beadb3eb28d2c931abd6f
PeterGreen8801 <petergreen880@yahoo.com>
2022-04-28 21:03:12 -0400

    Added other Countdown Scripts
    Added all other countdown scripts for the other grill sockets.

---
+++ VR Personal Project/VR Personal Project - Peter/Assets/Scripts/Countdown Timer Scripts/Countdown3.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Countdown3 : MonoBehaviour
+{
+    public float currentTime = 0f;
+    public float startingTime = 5f;
+
+    public bool timerIsRunning = false;
+
+    //public int holderPicker = 0;
+
+    [SerializeField]
+    TextMeshProUGUI countdownText;
+
+    [SerializeField]
+    GameObject textHolder;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            textHolder.SetActive(true);
+            currentTime = currentTime - 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
+
+            if (currentTime <= 0)
+            {
+                currentTime = startingTime;
+                textHolder.SetActive(false);
+                timerIsRunning = false;
+            }
+        }
+
+    }
+
+
+    /*
+    public void startTime(int textHolderToUse)
+    {
+        if (textHolderToUse == 1)
+        {
+            timerIsRunning = true;
+            //holderPicker = textHolderToUse;
+        }
+
+    }
+    */
+
+    public void startTimer()
+    {
+        timerIsRunning = true;
+    }
+}
